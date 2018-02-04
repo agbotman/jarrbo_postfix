2fec6f00770dfbee97551b9b913cd607ed535dcd
Ton Botman <ton@jarrbo.nl>
2018-02-04 18:51:11 +0100

    Add description fields to VirtualUser and VirtualAlias

--- models.py
+++ models.py
@@ -8,18 +8,34 @@ class VirtualDomain(models.Model):
 	"""
 	name = models.CharField(max_length=100, unique=True)
 	
+	def __str__(self):
+		return self.name
+	
 class VirtualUser(models.Model):
 	"""
 	Users for which email is handled by Postfix
 	"""
-	domain = models.ForeignKey('VirtualDomain')
-	email = models.EmailField
+	domain = models.ForeignKey('VirtualDomain', 
+								on_delete=models.CASCADE)
+	email = models.EmailField(unique=True)
 	password = models.CharField(max_length=150)
+	description = models.CharField(max_length=150, null=True, blank=True)
+	
+	def __str__(self):
+		return self.email
 	
 class VirtualAlias(models.Model):
 	"""
 	Aliases for email addresses in virtual domains
 	"""
-	domain = models.ForeignKey('VirtualDomain')
+	domain = models.ForeignKey('VirtualDomain',
+								on_delete=models.CASCADE)
 	source = models.CharField(max_length=100, unique=True)
 	destination = models.CharField(max_length=100, unique=True)
+	description = models.CharField(max_length=150, null=True, blank=True)
+	
+	class Meta:
+		verbose_name_plural = "Aliases"
+
+	def __str__(self):
+		return self.source
