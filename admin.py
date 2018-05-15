8da8c4913d74dbded5100dadfaaf984a1f6f847f
Ton Botman <ton@jarrbo.nl>
2018-05-15 12:15:01 +0200

    registered VirtualAliasAdmin class to show additional fields in admin

--- admin.py
+++ admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+class VirtualAliasAdmin(admin.ModelAdmin):
+	list_display = ('domain', 'source', 'destination', 'description')
+	list_editable = ('description',)
+
 admin.site.register(VirtualDomain)
 admin.site.register(VirtualUser)
-admin.site.register(VirtualAlias)
+admin.site.register(VirtualAlias, VirtualAliasAdmin)
